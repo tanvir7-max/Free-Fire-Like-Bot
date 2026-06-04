c97d5424bf522c89f8b19edd3df13b34b649d161
tanvir7-max <bgali7575@gmail.com>
2026-06-05 03:47:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found! Please set your bot token in environment variables.")
     sys.exit(1)
 
-REQUIRED_CHANNELS = ["@freefirefreelikepro1100"]
 GROUP_JOIN_LINK = "https://t.me/+zOe8s3Ij2dcyODJl"
 OWNER_ID = 7941543567
 OWNER_USERNAME = "@tanvir75759"
